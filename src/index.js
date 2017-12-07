4b5afea5a2669ccb078263047649a3fc2f8f03e7
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-07 15:46:44 +0200

    via "longest strike" map for each element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    const highScoresMap = {};
+    let Store = [];
+    const longestSliceLength = array.reduce((longestSliceLength, currValue) => {
+        const leftNeighbour = Store[currValue - 2];
+        const me = currValue;
+        const rightNeighbour = Store[currValue];
+
+        Store[currValue - 1] = currValue;
+        highScoresMap[me] = {
+            recordLength: 1,
+            starter: currValue
+        };
+
+        if (rightNeighbour in highScoresMap) {
+            highScoresMap[me] = highScoresMap[rightNeighbour];
+            highScoresMap[me].recordLength++;
+            highScoresMap[me].starter = me;
+        }
+        if (leftNeighbour in highScoresMap) {
+            highScoresMap[leftNeighbour].recordLength += highScoresMap[me].recordLength;
+            highScoresMap[me] = highScoresMap[leftNeighbour];
+        }
+
+        return Math.max.apply(null, [
+            longestSliceLength,
+            highScoresMap[me].recordLength
+        ]);
+    }, 0);
+
+    return longestSliceLength === 1
+        ? 0
+        : longestSliceLength;
+};
